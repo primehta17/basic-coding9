c1e84cf83d384e8e87fa293027eaf64ced6658a2
Priyanka Mehta <primehta17@gmail.com>
2025-04-03 12:16:20 +0530

    loop obj and put in array

---
+++ one.js
@@ -0,0 +1,17 @@
+// Loop Through Object Using for...in and put that in array
+let obj={
+    5:"abc",
+    2:"cgf",
+   steps: function(){
+        console.log("steps in fun")
+    }
+}
+
+let arr1=[],arr2=[];
+for(let i in obj){
+    console.log(obj[i])
+    arr2.push(i)
+    arr1.push(obj[i]);
+}
+console.log(arr1);
+console.log(arr2);
